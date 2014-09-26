9b6c96d59b23f16836adbbd4bd76f138722437b1
Matthew Bunday <zenceph@gmail.com>
2014-09-25 22:06:59 -0400

    Add bolded text

--- idea-filtering.js
+++ idea-filtering.js
@@ -2,16 +2,33 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault("counter", 0);
 
+  Template.entry.helpers({
+    bolded_text: function() {
+      text = this.text;
+      search_input = Session.get("search_input");
+      if (search_input) {
+        text = text.replace(new RegExp('<b>', 'gi'), '');
+        text = text.replace(new RegExp('</b>', 'gi'), '');
+        text = text.replace(new RegExp(search_input, 'gi'), '<b>' + search_input + '</b>')
+      }
+      return text;
+    }
+  })
+
   Template.entry_center.helpers({
     filtered_entries: function() {
-      return Entries.find();
+      if (Session.get("search_input")) {
+        return Entries.find({text: {"$regex": Session.get("search_input")}});
+      } else {
+        return Entries.find();
+      }
     }
   });
 
   Template.entry_center.events({
-    'click button': function () {
+    'keyup': function () {
       // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+      Session.set("search_input", $('input[name=search]').val());
     }
   });
 }
